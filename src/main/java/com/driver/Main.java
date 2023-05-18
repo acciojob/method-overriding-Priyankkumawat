be74153b4cd18fe2a748bf629ee67c1206bd6d07
Galactus__3000 <priyankkumawat7223@gmail.com>
2023-05-18 14:48:02 +0530

    v1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,14 +6,11 @@ class A{
     }
 }
 class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
+    public String meth(){return "Method is overridden in Extendend class B";}
 }
 public class Main {
     public static void main(String[] args) {
         B b=new B();
-        System.out.println(b.meth());
         System.out.println(b.meth());
     }
 }
